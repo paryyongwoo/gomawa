eded1ab5ea0e359b18fc042b2dd8fa9d2badfac4
paryyongwoo <apfhd5620@gmail.com>
2020-04-16 12:00:30 +0900

    멤버 테이블에 프로필이미지유알엘 추가

--- app/src/main/java/com/gomawa/dto/Member.java
+++ app/src/main/java/com/gomawa/dto/Member.java
@@ -4,14 +4,30 @@ import java.time.LocalDateTime;
 
 public class Member {
 
-    private Long key; // 네이버, 카카오 로그인에서 제공하는 식별자 id
+    // 네이버, 카카오 로그인에서 제공하는 식별자 id
+    private Long key;
+    // 이메일
     private String email;
+    // 성별
     private String gender;
-    private String nickName; // 닉네임
+    // 닉네임
+    private String nickName;
+    // 최초 로그인 시각
     private LocalDateTime regDate;
+    // 프로필 이미지 url
+    private String profileImgUrl;
 
     public Member() {
 
+    }
+
+    public Member(Long key, String email, String gender, String nickName, LocalDateTime regDate, String profileImgUrl) {
+        this.key = key;
+        this.email = email;
+        this.gender = gender;
+        this.nickName = nickName;
+        this.regDate = regDate;
+        this.profileImgUrl = profileImgUrl;
     }
 
     public Long getKey() {
@@ -50,6 +66,14 @@ public class Member {
         this.regDate = regDate;
     }
 
+    public String getProfileImgUrl() {
+        return profileImgUrl;
+    }
+
+    public void setProfileImgUrl(String profileImgUrl) {
+        this.profileImgUrl = profileImgUrl;
+    }
+
     @Override
     public String toString() {
         return "Member{" +
@@ -58,14 +82,7 @@ public class Member {
                 ", gender='" + gender + '\'' +
                 ", nickName='" + nickName + '\'' +
                 ", regDate=" + regDate +
+                ", profileImgUrl='" + profileImgUrl + '\'' +
                 '}';
-    }
-
-    public Member(Long key, String email, String gender, String nickName, LocalDateTime regDate) {
-        this.key = key;
-        this.email = email;
-        this.gender = gender;
-        this.nickName = nickName;
-        this.regDate = regDate;
     }
 }
